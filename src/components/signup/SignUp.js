6cc534e5f54ee3c90de4ef88752cd241fbc49d4d
sarwar-asik <shasik2021@gmail.com>
2022-10-17 16:01:44 +0600

    use Context

--- src/components/signup/SignUp.js
+++ src/components/signup/SignUp.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import './Signup.css'
 
 const SignUp = () => {
+    const [error ,setError] = useState(null);
+
+    const handleSubmit = event =>{
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+        console.log(email,password,confirm);
+
+        if(password.length<6){
+            setError("password should be 6 character")
+        }
+        if(password !== confirm){
+                setError('Your PassWord did not match');
+                return
+        }
+        setError('');
+        
+
+    }
   return (
     <div className="form-container">
       <h1 className="form-tittle">Sign Up </h1>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" required placeholder="Your Email" />
@@ -27,6 +49,7 @@ const SignUp = () => {
             required
             placeholder="Your password"
           />
+          <p className="text-error">{error}</p>
         </div>
         <input className="btn-submit" type="submit" value="Login" />
       </form>
--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,12 +1,18 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import app from '../firebase/config';
+import {getAuth} from 'firebase/auth';
 
 
 
 export const AuthContext  = createContext();
 
+const auth = getAuth(app);
+
 const UserContext = ({children}) => {
 
-    const user = {email: 'abcdefgh'}
+    const [user,setUser] = useState(null)
+
+  
     const authInfo = {user};
 
     return (
